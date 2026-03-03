a97adadca2e4717a50ee73fabf89752d9db0e112
benjamindecret-compositeur <benjamin.decret@gmail.com>
2026-03-03 14:32:52 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+    remotePatterns: [],
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig;
